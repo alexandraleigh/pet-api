09500a9f736b947c86a1668beac8cbb7fc2e1b40
Allie <alliebartholomew1@gmail.com>
2018-06-20 14:33:00 -0700

    Authenicate User

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -15,10 +15,7 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordInvalid do |e|
       json_response({ message: e.message }, :unprocessable_entity)
     end
-  end
 
-  # authenication
-  included do
     # custom handlers
     rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
     rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
